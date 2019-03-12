b21404fe9a684687cbab0ec3a6255489e1e1701e
nishidat <nishidat@isao.co.jp>
2019-03-12 20:57:09 +0900

    Add: Started!

--- nodeserver/app.js
+++
@@ -1,9 +0,0 @@
-// Setup basic express server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var port = process.env.PORT || 3000;
-
-server.listen(port, function () {
-	console.log('Server listening at port %d', port);
-});
